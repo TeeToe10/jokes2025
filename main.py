e5655c2cc628d891c95ad340a6af0e562cb8dfc4
TeeToe10 <urmomha6@gmail.com>
2026-02-02 15:28:08 +0000

    work

--- main.py
+++ main.py
@@ -3,35 +3,66 @@
 
 # make this performance task ready for submission
 # To give the user a fun experience hearing knock knock jokes
+import random
 
-joke = input("Do you want to hear a joke? ")
+
+jokes = [
+    {"category": "robbers",
+     "setup": "Knock Knock",
+     "who": "Calder",
+     "punchline": "Calder police — I've been robbed!"},
+
+    {"category": "tanks",
+     "setup": "Knock Knock",
+     "who": "Tank",
+     "punchline": "You're welcome!"},
+
+    {"category": "pencils",
+     "setup": "Knock Knock",
+     "who": "Broken pencil",
+     "punchline": "Nevermind… it's pointless!"}
+]
+
+
+def tell_joke(category):
+
+    matching = [j for j in jokes if j["category"] == category]
+
+    if not matching:
+        print("That’s not a valid category.")
+        return
+
+    
+    joke = random.choice(matching)
+
+    input(joke["setup"])
+    input(joke["who"])
+    print(joke["punchline"])
+
+
+
+joke = input("Do you want to hear a joke? (yes/no) ")
+
 if joke == "no":
-    print("Okay suit yourself!")
-while joke == "yes":
-    print("Great, Let's Play")
-    question = input("Do you want to hear a joke about robbers, tanks, or pencils? ")
-    if question == "robbers":
-        input("Knock Knock ")
-        input("Calder")
-        print("Calder police - I've been robbed!")
-        joke = input("Do you want to hear another joke or are you finished? ")
-    elif question == "tanks":
-        input("Knock Knock ")
-        input("Tank ")
-        input("You are welcome! ")
-        joke = input("Do you want to hear another joke or are you finished? ")
-    elif question == "pencils":
-        input("Knock Knock ")
-        input("Broken pencil ")
-        input("Nevermind, it's pointless! ")
-        joke = input("Do you want to hear another joke or are you finished? ")
+    print("Okay, suit yourself!")
+else:
+    while joke == "yes":
+        print("Great, let's play!")
+        category = input("Choose a joke: robbers, tanks, or pencils: ")
+
+        tell_joke(category)
+
+        joke = input("Do you want to hear another joke or are you finished? (yes/finished) ")
+
+
 if joke == "finished":
-    rate = int(input("Please rate our game 1-10! "))
-    final_score = int(rate * 10)
-    print(str(final_score) + " percent satisfaction rate")
-    friend = input("Would you recommend this game to a friend? ")
+    rate = int(input("Please rate our game 1–10: "))
+    final_score = rate * 10
+    print(str(final_score) + "% satisfaction rate!")
+
+    friend = input("Would you recommend this game to a friend? (yes/no/maybe) ")
 
     if friend == "yes" or friend == "maybe":
-        print("Thanks, we appreciate it. ")
+        print("Thanks, we appreciate it!")
     else:
-        print("Sorry you did not enjoy it. ")
+        print("Sorry you did not enjoy it.")
